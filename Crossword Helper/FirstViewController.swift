cf036643d7503a610319e9917a26e72752874bba
Justin Duke <me@jmduke.com>
2016-09-28 13:14:58 -0700

    Initial Commit

---
+++ Crossword Helper/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Crossword Helper
+//
+//  Created by Justin Duke on 9/28/16.
+//  Copyright © 2016 Pug and Chalice, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
